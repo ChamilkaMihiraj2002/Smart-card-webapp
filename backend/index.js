d275d16d84eccea63aed5e9e86a3a6b3b7d74b0c
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-02-18 23:43:24 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,28 @@
 require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Routes 
 const classRoutes = require('./routes/class.routes.js');
 const feeRoutes = require('./routes/fee.routes.js');
 const studentRoutes = require('./routes/student.routes.js');
 const accountingRoutes = require('./routes/accounting.routes.js');
-const userRouts = require('./routes/user.routes.js');
+const userRoutes = require('./routes/user.routes.js');
 const attendanceRoutes = require('./routes/attendance.routes.js');
+
 const app = express()
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // middleware configuration
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes configuration
-app.use('/api/users', userRouts);
+app.use('/api/users', userRoutes);
 app.use('/api/classes', classRoutes);
 app.use('/api/fees', feeRoutes);
 app.use('/api/students', studentRoutes);
