aaef4ac18e3554743d23e4da9f42b4f1a61fd8be
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-03-07 00:01:08 +0530

    Navbar

--- frontend/src/components/LoginAndRegister/Navbar.jsx
+++ frontend/src/components/LoginAndRegister/Navbar.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import { Layout, Button, Space } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Layout, Button, Space, Typography } from 'antd';
 import { Link } from 'react-router-dom';
-import { IdcardOutlined } from '@ant-design/icons';
+import { IdcardOutlined, UserOutlined } from '@ant-design/icons';
 import './Navbar.css';
 
 const { Header } = Layout;
+const { Title } = Typography;
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 20);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <Header className="navbar">
-      <div className="navbar-logo">
-        <IdcardOutlined className="logo-icon" />
-        <span className="logo-text">Smart Card</span>
+    <Header className={`navbar ${scrolled ? 'navbar-scrolled' : ''}`}>
+      <div className="navbar-container">
+        <Link to="" className="navbar-logo">
+          <div className="logo-animation">
+            <IdcardOutlined className="logo-icon" />
+          </div>
+          <Title level={4} className="logo-text">
+            SmartCard
+          </Title>
+        </Link>
+        <div className="nav-right">
+          <Space className="auth-buttons">
+            <Link to="/login">
+              <Button type="text" icon={<UserOutlined />} className="signin-btn">
+                Sign In
+              </Button>
+            </Link>
+            <Link to="http://localhost:5173/registration">
+              <Button type="primary" className="register-btn">
+                Get Started
+              </Button>
+            </Link>
+          </Space>
+        </div>
       </div>
-      <Space className="auth-buttons">
-        <Link to="/login">
-          <Button type="text">Sign In</Button>
-        </Link>
-        <Link to="/register">
-          <Button type="primary">Register</Button>
-        </Link>
-      </Space>
     </Header>
   );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Form, Input, Button, Card, Typography, message } from 'antd';
 import { UserOutlined, LockOutlined, LoginOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/LoginAndRegister/Navbar';
 import axios from 'axios'; // Make sure to install axios
 import './Login.css';
 
@@ -86,53 +87,56 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <Card className="login-card">
-        <div style={{ textAlign: 'center', marginBottom: '24px' }}>
-          <LoginOutlined style={{ fontSize: '48px', color: '#1890ff' }} />
-        </div>
-        <Title level={2} className="login-title">Welcome Back</Title>
-        <Form
-          name="login"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          layout="vertical"
-        >
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
-          >
-            <Input 
-              prefix={<UserOutlined />} 
-              placeholder="Username" 
-              size="large"
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password
-              prefix={<LockOutlined />}
-              placeholder="Password"
-              size="large"
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block size="large">
-              Log in
-            </Button>
-          </Form.Item>
-
-          <div style={{ textAlign: 'center' }}>
-            <Typography.Text type="secondary">
-              Don&apos;t have an account? <a href="http://localhost:5173/registration">Register here</a>
-            </Typography.Text>
+    <div>
+      <Navbar />
+      <div className="login-container">
+        <Card className="login-card">
+          <div style={{ textAlign: 'center', marginBottom: '24px' }}>
+            <LoginOutlined style={{ fontSize: '48px', color: '#1890ff' }} />
           </div>
-        </Form>
-      </Card>
+          <Title level={2} className="login-title">Welcome Back</Title>
+          <Form
+            name="login"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            layout="vertical"
+          >
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input 
+                prefix={<UserOutlined />} 
+                placeholder="Username" 
+                size="large"
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Password"
+                size="large"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block size="large">
+                Log in
+              </Button>
+            </Form.Item>
+
+            <div style={{ textAlign: 'center' }}>
+              <Typography.Text type="secondary">
+                Don&apos;t have an account? <a href="http://localhost:5173/registration">Register here</a>
+              </Typography.Text>
+            </div>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 };
